8e54e3246d135237b17a1eaa2d0bf20e49fb9f62
Loris Tichadou <loris.tichadou@outlook.com>
2022-01-09 16:40:13 +0100

    [NEW] add regex for youtube links, updated discordjs

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,50 @@
 //eslint-disable-next-line
 require("dotenv").config();
 
-import Discord from "discord.js";
+import { Client, Intents } from "discord.js";
 
-import { isApologies } from "./helpers";
+import { isApologies, isYoutubeLink } from "./helpers";
 // Create an instance of a Discord client
-const client = new Discord.Client();
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
+
+const threadId = "926909785117429861";
 
 const self = process.env.CLIENTID;
 
 const onMessageHandler = async (message) => {
-  const { author, content } = message;
+  const { channel, author, content } = message;
   // ignoring message from himself
-
   if (author.id === self) return;
 
   if (isApologies(content.toLowerCase())) {
-    await message.react("😄");
+    message.react("😄");
+  }
+
+  const thread = channel.isThread
+    ? null
+    : channel.threads.cache.find((x) => x.id === threadId);
+  if (thread && thread.joinable) await thread.join();
+
+  if (channel.id === threadId) {
+    //
+    const foundLink = isYoutubeLink(content);
+    if (foundLink)
+      message.reply(
+        `lien trouvé et ajouté à la playlist: <${isYoutubeLink(content)}>`
+      );
   }
 };
 
 // Create an event listener for messages
-client.on("message", onMessageHandler);
+client.on("messageCreate", onMessageHandler);
 
-client.on("ready", () => {
+client.once("ready", () => {
   console.log("I am ready!");
 });
 
--- src/helpers.js
+++ src/helpers.js
@@ -20,3 +20,11 @@ const apologies = [
 export const isApologies = (messageContent) => {
   return messageContent.split(" ").some((e) => apologies.includes(e));
 };
+
+const youtubeRegex = new RegExp(
+  /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w-_]+)/gim
+);
+export const isYoutubeLink = (messageContent) => {
+  const res = youtubeRegex.exec(messageContent);
+  return res ? res[0] : null;
+};
