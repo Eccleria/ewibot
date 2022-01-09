2a95908c661e0120fe46541034882b9a60287801
Loris Tichadou <loris.tichadou@outlook.com>
2022-01-09 13:14:32 +0100

    [NEW] add v1 apologies react

--- src/bot.js
+++ src/bot.js
@@ -3,22 +3,21 @@ require("dotenv").config();
 
 import Discord from "discord.js";
 
-// import { isCommand } from "./helpers";
+import { isApologies } from "./helpers";
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
 const self = process.env.CLIENTID;
 
 const onMessageHandler = async (message) => {
-  const { channel, author, content } = message;
+  const { author, content } = message;
   // ignoring message from himself
-  console.log(channel.threads);
 
-  if (!channel.threads) {
-    const chann = await channel.fetch();
-    console.log(chann);
-  }
   if (author.id === self) return;
+
+  if (isApologies(content)) {
+    await message.react("😄");
+  }
 };
 
 // Create an event listener for messages
--- src/helpers.js
+++ src/helpers.js
@@ -1,1 +1,17 @@
 export const isCommand = (content) => content[0] === "!";
+
+const apologies = [
+  "desolé",
+  "desole",
+  "dsl",
+  "sorry",
+  "sry",
+  "desoler",
+  "désolé",
+  "désoler",
+  "pardon",
+];
+
+export const isApologies = (messageContent) => {
+  return messageContent.split(" ").some((e) => apologies.includes(e));
+};
