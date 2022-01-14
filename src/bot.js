bf7b702d99a5afd5de989f81b89411565fe4c04a
Titch88 <loris.tichadou@outlook.com>
2022-01-14 08:35:24 +0100

    Feat/auto playlist spotify (#4)
    * [NEW] add regex for youtube links, updated discordjs
    
    * [NEW] add panduom react
    
    * [NEW] renaming for clarity, add conf depending on guildId
    
    * [NEW] added spotify auth, starting research
    
    * [NEW] add spotify link parsing
    
    * [NEW] add duplicated song detection
    
    * [NEW] add manual command
    
    * [NEW] remove console logs
    
    * [NEW] moved config in another file
    
    * [NEW] add reaction for deleting a song
    
    * [NEW] update gitignore
    
    * [NEW] add USE_SPOTIFY env variable
    
    * [NEW] add error handling
    
    Co-authored-by: Eccleria <debaud.ju@gmail.com>

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,113 @@
 //eslint-disable-next-line
 require("dotenv").config();
 
-import Discord from "discord.js";
+import { Client, Intents } from "discord.js";
+import SpotifyWebApi from "spotify-web-api-node";
+import {
+  isApologies,
+  parseLink,
+  checkIsOnThread,
+  deleteSongFromPlaylist,
+} from "./helpers";
+import { generateSpotifyClient } from "./spotifyHelper";
+import servers from "./servers";
 
-import { isApologies } from "./helpers";
 // Create an instance of a Discord client
-const client = new Discord.Client();
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
+
+client.playlistCachedMessages = [];
+
+if (process.env.USE_SPOTIFY === "yes") {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: "http://localhost:3001",
+  });
+  generateSpotifyClient(spotifyApi);
+  client.spotifyApi = spotifyApi;
+}
 
 const self = process.env.CLIENTID;
 
 const onMessageHandler = async (message) => {
-  const { author, content } = message;
-  // ignoring message from himself
+  const { channel, author, content } = message;
 
-  if (author.id === self) return;
+  const currentServer = servers.find(
+    ({ guildId }) => guildId === channel.guild.id
+  );
+
+  // ignoring message from himself
+  if (
+    author.id === self ||
+    !currentServer ||
+    (process.env.DEBUG === "yes" && currentServer.name === "prod")
+  )
+    return;
+
+  const { panDuomReactId, playlistThreadId } = currentServer;
 
   if (isApologies(content.toLowerCase())) {
-    await message.react("😄");
+    message.react(panDuomReactId);
+  }
+
+  checkIsOnThread(channel, playlistThreadId);
+
+  if (process.env.USE_SPOTIFY === "yes" && channel.id === playlistThreadId) {
+    //
+    const foundLink = await parseLink(content, client);
+    if (foundLink) {
+      const { answer, songId } = foundLink;
+      const newMessage = await message.reply(answer);
+      client.playlistCachedMessages = [
+        ...client.playlistCachedMessages,
+        { ...newMessage, songId },
+      ];
+    }
+  }
+};
+
+const onReactionHandler = async (messageReaction) => {
+  const { message, emoji, users } = messageReaction;
+  const currentServer = servers.find(
+    ({ guildId }) => guildId === message.channel.guild.id
+  );
+
+  const { removeFromPlaylistEmoji } = currentServer;
+
+  const foundMessage = client.playlistCachedMessages.find(
+    ({ id }) => id === message.id
+  );
+
+  if (
+    process.env.USE_SPOTIFY === "yes" &&
+    foundMessage &&
+    emoji.name === removeFromPlaylistEmoji &&
+    users.cache
+      .map((user) => user.id)
+      .includes(message.mentions.users.first().id)
+  ) {
+    const { songId } = foundMessage;
+
+    const result = await deleteSongFromPlaylist(songId, client);
+    client.playlistCachedMessages = client.playlistCachedMessages.filter(
+      ({ id }) => id !== message.id
+    );
+    await message.reply(result);
   }
 };
 
 // Create an event listener for messages
-client.on("message", onMessageHandler);
+client.on("messageCreate", onMessageHandler);
 
-client.on("ready", () => {
+client.on("messageReactionAdd", onReactionHandler);
+
+client.once("ready", () => {
   console.log("I am ready!");
 });
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,7 @@
+//eslint-disable-next-line
+require("dotenv").config();
+import ytdl from "ytdl-core";
+
 export const isCommand = (content) => content[0] === "!";
 
 const apologies = [
@@ -19,4 +23,144 @@ const apologies = [
 
 export const isApologies = (messageContent) => {
   return messageContent.split(" ").some((e) => apologies.includes(e));
+};
+
+export const checkIsOnThread = async (channel, threadId) => {
+  const thread = channel.isThread
+    ? null
+    : channel.threads.cache.find((x) => x.id === threadId);
+  if (thread && thread.joinable) await thread.join();
+};
+
+const youtubeRegex = new RegExp(
+  /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w-_]+)/gim
+);
+
+const sanitizeTitle = (title) => {
+  return title
+    .toLowerCase()
+    .replace(/\([^()]*\)/g, "")
+    .replace(/official music video/gi, "")
+    .replace(/official lyrics video/gi, "")
+    .replace(/official audio/gi, "")
+    .replace(/official video/gi, "")
+    .replace(/official/gi, "")
+    .replace(/| napalm records/gi, "")
+    .replace(/officiel/gi, "")
+    .replace(/ ost /gi, "")
+    .replace(/credits/gi, "")
+    .replace(/original soundtrack/gi, "");
+};
+
+const parseYoutubeLink = async (messageContent, client) => {
+  const isYoutubeLink = await youtubeRegex.exec(messageContent);
+
+  if (!isYoutubeLink) return null;
+
+  const youtubeLink = isYoutubeLink[0];
+
+  const infos = await ytdl.getInfo(youtubeLink);
+
+  const searchQuery =
+    infos.videoDetails &&
+    infos.videoDetails.media &&
+    infos.videoDetails.media.artist &&
+    infos.videoDetails.media.song
+      ? `${infos.videoDetails.media.song} ${infos.videoDetails.media.artist}`
+      : sanitizeTitle(infos.videoDetails.title);
+
+  const dataFound = await client.spotifyApi.searchTracks(searchQuery);
+  const { items } = dataFound.body.tracks;
+  return items && items[0] && items[0].uri;
+};
+
+const parseSpotifyLink = async (messageContent) => {
+  // https://open.spotify.com/track/3HctJdhy5CFb06NKIVp92U?si=cde5ab12a0f64fa6
+  const isSpotifyLink = messageContent.includes("open.spotify.com/track");
+
+  if (!isSpotifyLink) return null;
+
+  const spotifyId = messageContent
+    .split("open.spotify.com/track")[1]
+    .slice(1, "3HctJdhy5CFb06NKIVp92Ue".length);
+  return `spotify:track:${spotifyId}`;
+};
+
+const parseAddCommand = async (messageContent, client) => {
+  if (!messageContent.startsWith("!addsong")) return null;
+
+  const searchQuery = messageContent.split(" ").slice(1).join(" ");
+
+  const dataFound = await client.spotifyApi.searchTracks(searchQuery);
+  const { items } = dataFound.body.tracks;
+  return items && items[0] && items[0].uri;
+};
+
+export const parseLink = async (messageContent, client) => {
+  const songSpotify = await parseSpotifyLink(messageContent);
+  const songYoutube = await parseYoutubeLink(messageContent, client);
+  const songManual = await parseAddCommand(messageContent, client);
+
+  const songId = songSpotify || songYoutube || songManual;
+
+  console.log("ID : ", songId);
+  if (songId) {
+    // check if song is already in playlist
+
+    const currentPlaylist = await client.spotifyApi.getPlaylist(
+      process.env.SPOTIFY_PLAYLIST_ID
+    );
+
+    if (
+      currentPlaylist.body.tracks.items
+        .map(({ track }) => track.uri)
+        .includes(songId)
+    )
+      return {
+        answer: "Cette chanson est deja dans la playlist !",
+        songId: null,
+      };
+    try {
+      client.spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_PLAYLIST_ID, [
+        songId,
+      ]);
+
+      const {
+        body: { tracks },
+      } = await client.spotifyApi.getTracks([songId.split(":")[2]]);
+
+      const artists = tracks[0].artists.reduce(
+        (acc, { name }) => `${acc},  ${name}`,
+        ""
+      );
+
+      const result = `${tracks[0].name} ${artists}`;
+
+      // return null;
+      return {
+        answer: `Chanson ajoutée : ${result}. Vous pouvez react avec ❌ pour annuler cet ajout !`,
+        songId,
+      };
+    } catch {
+      return {
+        answer: "Erreur lors de l'ajout",
+        songId: null,
+      };
+    }
+  }
+
+  return null;
+};
+
+export const deleteSongFromPlaylist = async (songId, client) => {
+  const tracks = [{ uri: songId }];
+  try {
+    await client.spotifyApi.removeTracksFromPlaylist(
+      process.env.SPOTIFY_PLAYLIST_ID,
+      tracks
+    );
+    return "Chanson supprimée de la playlist";
+  } catch {
+    return "erreur lors de la suppression";
+  }
 };
