17ddc52268475f54ea874e4a23add40a15a8eed4
Titch88 <loris.tichadou@outlook.com>
2022-01-14 08:33:32 +0100

    Revert "Feat/auto playlist spotify (#2)"
    This reverts commit d38169412c431cc7eb8430f087a38e463268e911.

--- src/bot.js
+++ src/bot.js
@@ -1,109 +1,29 @@
 //eslint-disable-next-line
 require("dotenv").config();
 
-import { Client, Intents } from "discord.js";
-import SpotifyWebApi from "spotify-web-api-node";
-import {
-  isApologies,
-  parseLink,
-  checkIsOnThread,
-  deleteSongFromPlaylist,
-} from "./helpers";
-import { generateSpotifyClient } from "./spotifyHelper";
-import servers from "./servers";
+import Discord from "discord.js";
 
+import { isApologies } from "./helpers";
 // Create an instance of a Discord client
-const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  ],
-});
-
-let cachedMessages = [];
-
-if (process.env.USE_SPOTIFY === "yes") {
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: "http://localhost:3001",
-  });
-  generateSpotifyClient(spotifyApi);
-  client.spotifyApi = spotifyApi;
-}
+const client = new Discord.Client();
 
 const self = process.env.CLIENTID;
 
 const onMessageHandler = async (message) => {
-  const { channel, author, content } = message;
-
-  const currentServer = servers.find(
-    ({ guildId }) => guildId === channel.guild.id
-  );
-
+  const { author, content } = message;
   // ignoring message from himself
-  if (
-    author.id === self ||
-    !currentServer ||
-    (process.env.DEBUG === "yes" && currentServer.name === "prod")
-  )
-    return;
 
-  console.log(content);
-
-  const { panDuomReactId, playlistThreadId } = currentServer;
+  if (author.id === self) return;
 
   if (isApologies(content.toLowerCase())) {
-    message.react(panDuomReactId);
-  }
-
-  checkIsOnThread(channel, playlistThreadId);
-
-  if (process.env.USE_SPOTIFY === "yes" && channel.id === playlistThreadId) {
-    //
-    const foundLink = await parseLink(content, client);
-    if (foundLink) {
-      const { answer, songId } = foundLink;
-      const newMessage = await message.reply(answer);
-      cachedMessages = [...cachedMessages, { ...newMessage, songId }];
-    }
-  }
-};
-
-const onReactionHandler = async (messageReaction) => {
-  const { message, emoji, users } = messageReaction;
-  console.log(message.mentions);
-  const currentServer = servers.find(
-    ({ guildId }) => guildId === message.channel.guild.id
-  );
-
-  const { removeFromPlaylistEmoji } = currentServer;
-
-  const foundMessage = cachedMessages.find(({ id }) => id === message.id);
-
-  if (
-    process.env.USE_SPOTIFY === "yes" &&
-    foundMessage &&
-    emoji.name === removeFromPlaylistEmoji &&
-    users.cache
-      .map((user) => user.id)
-      .includes(message.mentions.users.first().id)
-  ) {
-    const { songId } = foundMessage;
-
-    const result = await deleteSongFromPlaylist(songId, client);
-    cachedMessages = cachedMessages.filter(({ id }) => id !== message.id);
-    await message.reply(result);
+    await message.react("😄");
   }
 };
 
 // Create an event listener for messages
-client.on("messageCreate", onMessageHandler);
+client.on("message", onMessageHandler);
 
-client.on("messageReactionAdd", onReactionHandler);
-
-client.once("ready", () => {
+client.on("ready", () => {
   console.log("I am ready!");
 });
 
