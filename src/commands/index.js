f4950f56236cffacba6f1991ed8d9300dbc8e116
Eccleria <debaud.ju@gmail.com>
2022-01-20 00:00:49 +0100

    [NEW] help command added

--- src/commands/index.js
+++ src/commands/index.js
@@ -5,20 +5,12 @@ import {
 } from "../helpers/index.js";
 
 const helloWorld = {
-  name: "hello world",
-  trigger: "!hello",
+  name: "helloWorld",
+  trigger: "!helloWorld",
   action: async (message /* client */) => {
     await message.channel.send("hello, world !");
   },
-  help: "une commande qui permet de dire bonjour",
-};
-
-const help = {
-  name: "help",
-  trigger: "!help",
-  action: async (message) => {
-    await message.channel.send("commande d'aide, à venir");
-  },
+  help: "Cette commande permet de dire bonjour",
 };
 
 const ignore = {
@@ -30,13 +22,33 @@ const ignore = {
     const authorId = message.author.id;
     if (getIgnoredUsers(db).includes(authorId)) {
       removeIgnoredUser(authorId, db);
-      await message.channel.send("Je ne vous ignore plus");
+      await message.channel.send("Je vais de nouveau réagir à vos messages.");
     } else {
       addIgnoredUser(authorId, db);
-      await message.channel.send("Dorénavant je vous ignore");
+      await message.channel.send("Dorénavant je ne réagirai plus à vos message.");
     }
   },
-  help: "Une commande qui empêche Ewibot de réagir à vos messages",
+  help: "Cette commande empêche ou non Ewibot de réagir à vos messages.",
 };
 
-export default [helloWorld, help, ignore];
+const commands = [helloWorld, ignore];
+
+const help = {
+  name: "help",
+  trigger: "!help",
+  action: async (message) => {
+    const words = message.content.split(" ");
+    if (words.length === 1) await message.channel.send("Cette commande permet d'afficher l'aide \
+d'une commande. Pour obtenir l'aide d'une commande 'ex', tapez !help ex");
+    else {
+      const command = commands.find(cmd => cmd.name === words[1]);
+      if (command === undefined) {
+        await message.channel.send(`La commande "${words[1]}" n'existe pas.`);
+        return
+      }
+      await message.channel.send(command.help);
+    }
+  },
+};
+
+export default [...commands, help];
