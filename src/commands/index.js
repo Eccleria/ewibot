67aa4271ccfc7f413f60f7c4461c3704579f5057
Eccleria <debaud.ju@gmail.com>
2022-01-22 21:30:14 +0100

    [NEW] removed trigger element from commands. Modified command recognition using now name element.

--- src/commands/index.js
+++ src/commands/index.js
@@ -6,7 +6,6 @@ import {
 
 const helloWorld = {
   name: "helloWorld",
-  trigger: "!helloWorld",
   action: async (message /* client */) => {
     await message.channel.send("hello, world !");
   },
@@ -15,7 +14,6 @@ const helloWorld = {
 
 const ignore = {
   name: "ignore",
-  trigger: "!ignore",
   action: async (message, client) => {
     const db = client.db;
     console.log("DB", db);
@@ -25,7 +23,9 @@ const ignore = {
       await message.channel.send("Je vais de nouveau réagir à vos messages.");
     } else {
       addIgnoredUser(authorId, db);
-      await message.channel.send("Dorénavant je ne réagirai plus à vos message.");
+      await message.channel.send(
+        "Dorénavant je ne réagirai plus à vos message."
+      );
     }
   },
   help: "Cette commande empêche ou non Ewibot de réagir à vos messages.",
@@ -35,21 +35,23 @@ const commands = [helloWorld, ignore];
 
 const help = {
   name: "help",
-  trigger: "!help",
   action: async (message) => {
     const words = message.content.split(" ");
     if (words.length === 1) {
-      const helpText = commands.reduce(((acc, cur) => { return acc.concat(`\n- ${cur.name}`) }),
+      const helpText = commands.reduce(
+        (acc, cur) => {
+          return acc.concat(`\n- ${cur.name}`);
+        },
         `Cette commande permet d'afficher l'aide d'une commande. Pour obtenir l'aide \
 d'une commande 'ex', tapez !help ex. \nPour le moment, les commandes suivantes ont été \
-implémentées :\n- help`);
+implémentées :\n- help`
+      );
       await message.channel.send(helpText);
-    }
-    else {
-      const command = commands.find(cmd => cmd.name === words[1]);
+    } else {
+      const command = commands.find((cmd) => cmd.name === words[1]);
       if (command === undefined) {
         await message.channel.send(`La commande "${words[1]}" n'existe pas.`);
-        return
+        return;
       }
       await message.channel.send(command.help);
     }
