c0f49fbd9136f1b2597204d4a9f5d1d4a3c7969e
Eccleria <debaud.ju@gmail.com>
2022-01-28 15:11:36 +0100

    [NEW] added dbHelper basic functions & db element.

--- src/helpers/dbHelper.js
+++ src/helpers/dbHelper.js
@@ -25,3 +25,31 @@ const getIgnoredUsers = (db) => {
 };
 
 export { addIgnoredUser, removeIgnoredUser, getIgnoredUsers, isUserIgnored };
+
+const addBirthday = async (message, db) => {
+  const words = message.content.toLowerCase().split(" ");
+  const birthday = words[1];
+  if (!isUserBirthday(message, db)) {
+    db.data.birthdayUsers = [...db.data.birthdayUsers, { "userId": message.author.id, "userBirthday": birthday}];
+    await db.write();
+  }
+};
+
+const removeBirthday = async (message, db) => {
+  if (isUserBirthday(message, db)) {
+    db.data.birthdayUsers = db.data.birthdayUsers.filter(
+      ({ userId }) => userId !== message.author.id
+    );
+    await db.write();
+  }
+};
+
+const isUserBirthday = (message, db) => {
+  return db.data.birthdayUsers.map(obj => {return obj.userId}).includes(message.author.id)
+};
+
+const getBirthday = (db) => {
+  return db.data.birthdayUsers;
+};
+
+export { addBirthday, removeBirthday, isUserBirthday, getBirthday };
