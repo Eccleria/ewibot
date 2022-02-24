298590856a22b4d8a75d9a80ae924eb5105e33c9
Eccleria <debaud.ju@gmail.com>
2022-02-24 22:45:54 +0100

    [NEW] changes in birthay file

--- src/commands/birthday.js
+++ src/commands/birthday.js
@@ -1,7 +1,6 @@
 import dayjs from "dayjs";
 import CustomParseFormat from "dayjs/plugin/customParseFormat.js";
 import "dayjs/locale/fr.js";
-
 dayjs.locale("fr");
 dayjs.extend(CustomParseFormat);
 
@@ -11,11 +10,9 @@ import {
   removeBirthday,
   getBirthday,
 } from "../helpers/index.js";
-
 import personnalities from "../personnalities.json";
 
 const PERSONNALITY = personnalities.normal;
-
 const replies = PERSONNALITY.commands.birthday;
 
 export const wishBirthday = async (db, channel) => {
@@ -40,49 +37,51 @@ export const wishBirthday = async (db, channel) => {
   }
 };
 
+const action = async (message, client) => {
+  const content = message.content;
+  const authorId = message.author.id;
+  const db = client.db;
+
+  const words = content.toLowerCase().split(" ");
+
+  if (words[1] && words[1] === "del") {
+    if (isbirthdayDate(authorId, db)) {
+      removeBirthday(authorId, db);
+      await message.reply(replies.removeUser);
+      return;
+    }
+  } else if (words[1] === "add" && words[2]) {
+    const date = dayjs(words[2], "DD-MM-YYYY")
+      .hour(8)
+      .minute(0)
+      .second(0)
+      .millisecond(0); // 8AM, local hour
+
+    if (date.isValid()) {
+      if (date.year() < 1950) {
+        await message.reply(replies.tooOld);
+      } else if (date.year() > dayjs().subtract(5, "year").year()) {
+        await message.reply(replies.tooYoung);
+      } else {
+        addBirthday(authorId, db, date.toISOString());
+        await message.reply(replies.addUser);
+      }
+    } else await message.reply(replies.parsingError);
+  } else if (words.length === 1) {
+    const birthdays = getBirthday(db).users;
+    const user = birthdays.find(({ userId }) => userId === authorId);
+
+    if (user)
+      await message.reply(
+        `${replies.getUser}${dayjs(user.birthdayDate).format("DD/MM/YYYY")}.`
+      );
+    else await message.reply(replies.userNotFound);
+  }
+};
+
 const birthday = {
   name: "birthday",
-  action: async (message, client) => {
-    const content = message.content;
-    const authorId = message.author.id;
-    const db = client.db;
-
-    const words = content.toLowerCase().split(" ");
-
-    if (words[1] && words[1] === "del") {
-      if (isbirthdayDate(authorId, db)) {
-        removeBirthday(authorId, db);
-        await message.reply(replies.removeUser);
-        return;
-      }
-    } else if (words[1] === "add" && words[2]) {
-      const date = dayjs(words[2], "DD-MM-YYYY")
-        .hour(8)
-        .minute(0)
-        .second(0)
-        .millisecond(0); // 8AM, local hour
-
-      if (date.isValid()) {
-        if (date.year() < 1950) {
-          await message.reply(replies.tooOld);
-        } else if (date.year() > dayjs().subtract(5, "year").year()) {
-          await message.reply(replies.tooYoung);
-        } else {
-          addBirthday(authorId, db, date.toISOString());
-          await message.reply(replies.addUser);
-        }
-      } else await message.reply(replies.parsingError);
-    } else if (words.length === 1) {
-      const birthdays = getBirthday(db).users;
-      const user = birthdays.find(({ userId }) => userId === authorId);
-
-      if (user)
-        await message.reply(
-          `${replies.getUser}${dayjs(user.birthdayDate).format("DD/MM/YYYY")}.`
-        );
-      else await message.reply(replies.userNotFound);
-    }
-  },
+  action,
   help: () => {
     return replies.help;
   },
