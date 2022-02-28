8681f978a18b1a99f8c459042244ff144872297c
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-02-28 19:36:11 +0100

    feat/roll_dice (#39)
    * [NEW] add roll command
    
    * [NEW] prettier + add reduce
    
    Co-authored-by: Loris TICHADOU <loris.tichadou@outlook.com>

--- src/commands/index.js
+++ src/commands/index.js
@@ -67,7 +67,49 @@ const ignoreChannel = {
   admin: true,
 };
 
-const commands = [helloWorld, ignore, reminder, birthday, ignoreChannel];
+const roll = {
+  name: "roll",
+  action: async (message, personality) => {
+    const args = message.content.toLowerCase().split(" ");
+    if (args[1]) {
+      const diceNumbers = args[1].split("d").map((nb) => Number(nb));
+      if (isNaN(diceNumbers[0]) && isNaN(diceNumbers[1]))
+        await message.reply(personality.roll.parsingError);
+      else if (
+        diceNumbers[0] > 20 ||
+        diceNumbers[1] > 100 ||
+        diceNumbers[0] <= 0 ||
+        diceNumbers[1] <= 0
+      ) {
+        await message.reply(personality.roll.numberError);
+      } else {
+        const diceNumber = diceNumbers[0];
+        const diceValue = diceNumbers[1];
+        const { total, details } = Array.from(new Array(diceNumber)).reduce(
+          (acc) => {
+            const value = Math.round(diceValue * Math.random()) + 1;
+            return {
+              total: acc.total + value,
+              details: [...acc.details, value],
+            };
+          },
+          {
+            total: 0,
+            details: [],
+          }
+        );
+
+        await message.reply(`${total} (${details.join(", ")})`);
+      }
+    }
+  },
+  help: () => {
+    return personnalities.normal.commands.roll.help;
+  },
+  admin: false,
+};
+
+const commands = [helloWorld, ignore, reminder, birthday, ignoreChannel, roll];
 
 const help = {
   name: "help",
@@ -81,9 +123,7 @@ const help = {
       await message.channel.send(`${baseText} - ${helpText.slice(0, -2)}`);
     } else {
       if (words[1] === "help") await message.channel.send(help.help());
-      const command = commands.find(
-        (cmd) => cmd.name === words[1]
-      );
+      const command = commands.find((cmd) => cmd.name === words[1]);
       if (!command || (!isAdmin(message.author.id) && command.admin)) {
         return;
       }
