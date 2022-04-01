829f8382994af05aafa411b046799758d00efa00
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-04-01 11:45:05 +0200

    Feat/reorganization (#38)
    * [NEW] relocate spotify functions
    
    * [NEW] changes in birthay file
    
    * [NEW] dbHelper reorganization
    
    * [NEW] birthday simplification && prettier
    
    * [NEW] personality rename
    
    * [FIX] fix dbHelper && commands
    
    * [NEW] modify servers.json name for coherence
    
    * [REBASE] add recent changes from master + few typos
    
    * [NEW] readme draft
    
    * [NEW] moved .json files && file access corrections
    
    * [NEW] minor commons.json correction
    
    * [NEW] add commentaries
    
    * [NEW] add personality admin command
    
    * [NEW] prettier
    
    * [NEW] add draft spotify command
    
    * [NEW] correct config.yml version error
    
    * [FIX] fix personality merging error
    
    * [NEW] add commentaries and correct import paths
    
    * [NEW] prettier
    
    * [NEW] simplify bot.js file
    
    * [FIX] fix birthday import loop
    
    * [NEW] spotify functions reorganization
    
    * [NEW] add personality class
    
    * [NEW] simplify reactionHandler
    
    * [NEW] add more apology intelligence + cleaning
    
    * [NEW] add the possibility to not add the birthday year + prettier
    
    * [NEW] add hungry reaction
    
    * [NEW] add edouin react when bot is concreted
    
    * [NEW] moved personnality, reorder
    
    * [NEW] fixed help command, renamed listeners functions
    
    * [NEW] remove reminder DM for further fix + console
    
    * [NEW] minor fixes
    
    Co-authored-by: Loris Tichadou <loris.tichadou@outlook.com>

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,88 +1,95 @@
 import {
+  //dbHelper
   isIgnoredChannel,
   removeIgnoredChannel,
   addIgnoredChannel,
-} from "../helpers/dbHelper.js";
-import {
   addIgnoredUser,
   removeIgnoredUser,
-  getIgnoredUsers,
+  //utils
   isAdmin,
 } from "../helpers/index.js";
+
 import reminder from "./reminder.js";
 import birthday from "./birthday.js";
 import concrete from "./concrete.js";
-import personnalities from "../personnalities.json";
+import { PERSONALITY } from "../personality.js";
 
 const helloWorld = {
+  // Is useful to verify is Ewibot is active or not.
   name: "ping",
-  action: async (message, personality) => {
-    await message.channel.send(personality.helloWorld.pong);
+  action: async (message) => {
+    await message.channel.send(PERSONALITY.getCommands().helloWorld.pong);
   },
   help: () => {
-    return personnalities.normal.commands.helloWorld.help;
+    return PERSONALITY.getCommands().helloWorld.help;
   },
   admin: false,
 };
 
 const ignore = {
+  // Allows users to choose if they want Ewibot reacting or not to their messages.
   name: "ignore",
-  action: async (message, personality, client) => {
+  action: async (message, client) => {
     const db = client.db;
     const authorId = message.author.id;
-    if (getIgnoredUsers(db).includes(authorId)) {
+    if (db.data.ignoredUsersIds.includes(authorId)) {
       removeIgnoredUser(authorId, db);
-      await message.channel.send(personality.ignore.notIgnored);
+      await message.channel.send(PERSONALITY.getCommands().ignore.notIgnored);
     } else {
       addIgnoredUser(authorId, db);
-      await message.channel.send(personality.ignore.ignored);
+      await message.channel.send(PERSONALITY.getCommands().ignore.ignored);
     }
   },
   help: () => {
-    return personnalities.normal.commands.ignore.help;
+    return PERSONALITY.getCommands().ignore.help;
   },
   admin: false,
 };
 
 const ignoreChannel = {
+  // ADMIN Allows to add or remove channels where Ewibot will (or not) react.
   name: "ignoreChannel",
-  action: async (message, personality, client) => {
+  action: async (message, client) => {
     const db = client.db;
     const ignoredChannelId =
       message.content.toLowerCase().split(" ")[1] || message.channel.id;
     if (isIgnoredChannel(db, ignoredChannelId)) {
       removeIgnoredChannel(db, ignoredChannelId);
       await message.reply(
-        personality.ignoreChannel.notIgnored.concat(`<#${ignoredChannelId}>.`)
+        PERSONALITY.getCommands().ignoreChannel.notIgnored +
+          `<#${ignoredChannelId}>.`
       );
     } else {
       addIgnoredChannel(db, ignoredChannelId);
       await message.reply(
-        personality.ignoreChannel.ignored.concat(`<#${ignoredChannelId}>.`)
+        PERSONALITY.getCommands().ignoreChannel.ignored +
+          `<#${ignoredChannelId}>.`
       );
     }
   },
   help: () => {
-    return personnalities.normal.commands.ignoreChannel.help;
+    return PERSONALITY.getCommands().ignoreChannel.help;
   },
   admin: true,
 };
 
 const roll = {
+  // Allow to get the total and each individual results for dice rolls.
   name: "roll",
-  action: async (message, personality) => {
+  action: async (message) => {
     const args = message.content.toLowerCase().split(" ");
     if (args[1]) {
+      //if enough args
       const diceNumbers = args[1].split("d").map((nb) => Number(nb));
       if (isNaN(diceNumbers[0]) || isNaN(diceNumbers[1]))
-        await message.reply(personality.roll.parsingError);
+        await message.reply(PERSONALITY.getCommands().roll.parsingError);
       else if (
         diceNumbers[0] > 20 ||
         diceNumbers[1] > 100 ||
         diceNumbers[0] <= 0 ||
         diceNumbers[1] <= 0
       ) {
-        await message.reply(personality.roll.numberError);
+        await message.reply(PERSONALITY.getCommands().roll.numberError);
       } else {
         const diceNumber = diceNumbers[0];
         const diceValue = diceNumbers[1];
@@ -105,44 +112,51 @@ const roll = {
     }
   },
   help: () => {
-    return personnalities.normal.commands.roll.help;
+    return PERSONALITY.getCommands().roll.help;
   },
   admin: false,
 };
 
 const commands = [
+  birthday,
+  concrete,
   helloWorld,
   ignore,
-  reminder,
-  birthday,
   ignoreChannel,
+  reminder,
   roll,
-  concrete,
 ];
 
 const help = {
   name: "help",
-  action: async (message, personality) => {
+  action: async (message) => {
     const words = message.content.split(" ");
     if (words.length === 1) {
-      const baseText = personality.help.init;
+      //$help
+      const baseText = PERSONALITY.getCommands().help.init;
       const helpText = commands.reduce((acc, cur) => {
         return `${cur.admin ? "_[admin]_ " : ""}${cur.name}, ${acc}`;
       }, "");
       await message.channel.send(`${baseText} - ${helpText.slice(0, -2)}`);
     } else {
-      if (words[1] === "help") await message.channel.send(help.help());
+      //$help help
+      if (words[1] === "help") {
+        await message.channel.send(help.help());
+        return;
+      }
+      //$help command
       const command = commands.find((cmd) => cmd.name === words[1]);
       if (!command || (!isAdmin(message.author.id) && command.admin)) {
+        //if user doesn't have the rigths
         return;
       }
       await message.channel.send(command.help());
     }
   },
   help: () => {
-    return personnalities.normal.commands.help.help;
+    return PERSONALITY.getCommands().help.help;
   },
   admin: false,
 };
 
-export default [...commands, help];
+export default [...commands, help]; // Regroups all commands
