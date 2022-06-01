35ea6201880cb26044c304c8632794fb3ebeca82
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-06-01 10:25:28 +0200

    [FIX] minor changes

--- src/personality.js
+++ src/personality.js
@@ -1,6 +1,6 @@
 // jsons imports
 import { readFileSync } from "fs";
-const personalities = JSON.parse(readFileSync("./static/personalities.json"));
+const personalities = JSON.parse(readFileSync("static/personalities.json"));
 
 //export var PERSONALITY = personalities.normal; // common var for all files
 
