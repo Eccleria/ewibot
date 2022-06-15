3c674f804d25735f01f5812bc858c87d4c5ae0c5
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-06-15 15:59:14 +0200

    [NEW] draft

--- src/admin/alavirien.js
+++ src/admin/alavirien.js
@@ -5,30 +5,31 @@ import { setupEmbed, finishEmbed } from "./utils.js";
 import { PERSONALITY } from "../personality.js";
 
 export const checkAlavirien = async (client, server, logChannel) => {
-  const db = client.db;
+  //function to check every alavirien in db if they meet the requirements
 
+  const db = client.db;
   const dbUsers = db.data.alavirien;
-  if (!dbUsers) return;
+  if (!dbUsers) return; //if no data in db, nothing to do
 
   //get personality
   const personality = PERSONALITY.getAdmin(); //get personality
   const alavirien = personality.alavirien;
 
-  const today = dayjs();
+  const today = dayjs(); //get today date
+
   dbUsers.forEach(async (cur) => {
-    //{ userId: authorId, messageNumber: number, joinAt: date}
+    //db data format : { userId: authorId, messageNumber: number, joinAt: date}
     const { userId, messageNumber, joinAt } = cur
     const day = dayjs(joinAt);
     const deltaT = today.diff(day); //diff between joining date and now in ms
 
-    //check if 1 week + 20 messages
+    //check if 1 week + 20 messages : alavirien requirement
     const isOneWeek = deltaT > 604800000; // 1week = 7*24*3600*1000 ms
     const is20Messages = messageNumber >= 20; 
     if (isOneWeek && is20Messages) {
       //get GuildMember
       const guild = await client.guilds.fetch(server.guildId);
       const guildMember = await guild.members.fetch(userId);
-      //console.log(guildMember);
 
       guildMember.roles.add(server.alavirienRoleId); //add role
       removeAlavirien(db, userId); //remove from db
@@ -37,6 +38,7 @@ export const checkAlavirien = async (client, server, logChannel) => {
       const embed = setupEmbed("DARK_GREY", alavirien, guildMember.user, "tag");
       await finishEmbed(alavirien, `<@${process.env.CLIENTID}>`, embed, logChannel);
     }
+    //if doesn't respect requirements, nothing to do
   })
 }
 
