47a4761e88a019822b66cec456d56b31c63d70f1
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-06-18 21:05:28 +0200

    [UPDATE] simplify code

--- src/commands/pronouns.js
+++ src/commands/pronouns.js
@@ -12,61 +12,35 @@ const action = async (message, client) => {
   //create all buttons
   const style = "SECONDARY";
   const row1 = new MessageActionRow().addComponents(
-    new MessageButton()
-      .setCustomId("il")
-      .setLabel(pronounsP.he)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("elle")
-      .setLabel(pronounsP.she)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("iel")
-      .setLabel(pronounsP.they)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("�l")
-      .setLabel(pronounsP.ael)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("askP")
-      .setLabel(pronounsP.ask)
-      .setStyle(style)
+    createButton("he", pronounsP.he, style),
+    createButton("she", pronounsP.she, style),
+    createButton("they", pronounsP.they, style),
+    createButton("ael", pronounsP.ael, style),
+    createButton("askP", pronounsP.askP, style)
   );
   const row2 = new MessageActionRow().addComponents(
-    new MessageButton()
-      .setCustomId("no")
-      .setLabel(pronounsP.no)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("all")
-      .setLabel(pronounsP.all)
-      .setStyle(style)
+    createButton("no", pronounsP.no, style),
+    createButton("all", pronounsP.all, style),
   );
   const rowsPronouns = [row1, row2];
 
   const rowAgreement = new MessageActionRow().addComponents(
-    new MessageButton()
-      .setCustomId("male")
-      .setLabel(pronounsP.male)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("neutral")
-      .setLabel(pronounsP.neutral)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("female")
-      .setLabel(pronounsP.female)
-      .setStyle(style),
-    new MessageButton()
-      .setCustomId("askA")
-      .setLabel(pronounsP.ask)
-      .setStyle(style)
+    createButton("male", pronounsP.male, style),
+    createButton("neutral", pronounsP.neutral, style),
+    createButton("female", pronounsP.female, style),
+    createButton("askA", pronounsP.askA, style),
   );
 
   //message.reply({components: [row1, row2, row3]})
   //send messages
 };
+
+const createButton = (id, label, style) => {
+  return new MessageButton()
+    .setCustomId(id)
+    .setLabel(label)
+    .setStyle(style);
+}
 
 const pronouns = {
   name: "pronouns",
