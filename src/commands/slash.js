3181ae6c756156d5d820459c97043cc5b2f29e5f
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-07-29 15:27:45 +0200

    [NEW] add ping + help (/) commands

--- src/commands/slash.js
+++ src/commands/slash.js
@@ -4,44 +4,64 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 
 import { PERSONALITY } from "../personality.js";
 
-
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-/*
-const helloWorld = {
-  // Is useful to verify is Ewibot is active or not.
-  name: "ping",
-  action: async (message) => {
-    await message.channel.send(PERSONALITY.getCommands().helloWorld.pong);
-  },
-  help: () => {
-    return PERSONALITY.getCommands().helloWorld.help;
-  },
-  admin: false,
-};
-*/
+const personality = PERSONALITY.getCommands();
 
-const helloWorld = {
+const ping = {
   command: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Ping !'),
-  action: async (interaction) => {
-    return interaction.reply(PERSONALITY.getCommands().helloWorld.pong);
-  }
+  action: (interaction) => {
+    interaction.reply(personality.helloWorld.pong);
+  },
+  help: (interaction) => {
+    interaction.reply({ content: personality.helloWorld.help, ephemeral: true });
+  },
 };
 
-const commands = [helloWorld.command.toJSON()];
+const helpCommands = [ping];
+const helpOptions = helpCommands.reduce((acc, cur) => {
+  const cmd = cur.command;
+  return [...acc, { name: cmd.name, value: cmd.name }]
+}, []);
 
-export const slashCommandsInit = async (self, guildId) => {
+//console.log(helpOptions);
+const help = {
+  command: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription("Cette commande permet d'afficher l'aide d'une commande.")
+    .addStringOption((option) =>
+      option
+        .setName('command')
+        .setDescription("Choix de la commande dont l'aide sera affiché.")
+        .addChoices(...helpOptions, { name: "help", value: "help" })
+        .setRequired(true)),
+  action: (interaction) => {
+    const userOption = interaction.options.getString("command"); //get option given by user
+    const foundCommand = slashCommands.find((cmd) => cmd.command.name === userOption); //find associated command
+
+    if (foundCommand) foundCommand.help(interaction); //execute foundCommand help()
+  },
+  help: (interaction) => {
+    interaction.reply({ content: personality.help.help, ephemeral: true });
+  },
+};
+
+const slashCommands = [ping, help]; //command + action
+
+export const slashCommandsInit = async (self, guildId, client) => {
   try {
     console.log('Started refreshing application (/) commands.');
 
     await rest.put(
       Routes.applicationGuildCommands(self, guildId),
-      { body: commands },
+      { body: slashCommands.map((cmd) => cmd.command.toJSON())},
     );
 
     console.log('Successfully reloaded application (/) commands.');
+
+    client.slashCommands = slashCommands; //save in client
   } catch (error) {
     console.error(error);
   }
