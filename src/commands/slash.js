95b487be106eb1f0c7f0094c9ff0ec7c0abe642f
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-08-18 16:03:10 +0200

    [NEW] add roll / command

--- src/commands/slash.js
+++ src/commands/slash.js
@@ -2,6 +2,7 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import { SlashCommandBuilder } from "@discordjs/builders";
 
+
 import { PERSONALITY } from "../personality.js";
 
 const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
@@ -21,7 +22,53 @@ const ping = {
   },
 };
 
-const helpCommands = [ping];
+const roll = {
+  command: new SlashCommandBuilder()
+    .setName("roll")
+    .setDescription("Lancer de dés")
+    .addIntegerOption((option) =>
+      option
+        .setName("dés")
+        .setDescription("Nombre de dés à lancer")
+        .setRequired(true)
+        .setMinValue(1)
+        .setMaxValue(20)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("faces")
+        .setDescription("Nombre de faces à chaque dé")
+        .setRequired(true)
+        .setMinValue(1)
+        .setMaxValue(100)
+    ),
+  action: async (interaction) => {
+    const dice = interaction.options.getInteger("dés");
+    const faces = interaction.options.getInteger("faces");
+    if (dice && faces) {
+      //if enough args
+      const dicesArray = Array.from(new Array(dice)); //create an array with enough dices
+      const { total, details } = dicesArray.reduce((acc) => {
+            const value = Math.round((faces - 1) * Math.random()) + 1;
+            return {
+                total: acc.total + value,
+                details: [...acc.details, value],
+              };
+          }, { total: 0, details: [] }
+        ); //compute total + each dices values
+      
+      await interaction.reply({ content: `${total} (${details.join(", ")})`, ephemeral: true });
+      }
+    },
+  help: (interaction) => {
+    interaction.reply({
+      content: personality.helloWorld.help,
+      ephemeral: true,
+    });
+  },
+}
+
+const helpCommands = [ping, roll];
 const helpOptions = helpCommands.reduce((acc, cur) => {
   const cmd = cur.command;
   return [...acc, { name: cmd.name, value: cmd.name }];
@@ -52,7 +99,7 @@ const help = {
   },
 };
 
-const slashCommands = [ping, help]; //command + action
+const slashCommands = [...helpCommands, help]; //command + action
 
 export const slashCommandsInit = async (self, guildId, client) => {
   try {
