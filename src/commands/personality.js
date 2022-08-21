941d17a3597e6f3ea2e9f2ff315282004f55ff1b
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-08-21 14:19:24 +0200

    [NEW] create / command

--- src/commands/personality.js
+++ src/commands/personality.js
@@ -1,17 +1,43 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
 // jsons imports
 import { readFileSync } from "fs";
 const personalities = JSON.parse(readFileSync("static/personalities.json"));
 
+const getPersonalityNames = () => {
+  const nameList = Object.keys(personalities); // list of all personalities names
+  const filtered = nameList.filter((str) => str !== "admin");
+  return filtered;
+}
+
+const commandChoices = () => {
+  const nameList = getPersonalityNames();
+  const result = nameList.reduce((acc, cur) => {
+    return [...acc, { name: cur, value: cur }];
+  }, []);
+
+  return result;
+}
+
+const command = new SlashCommandBuilder()
+  .setName("personality")
+  .setDescription("Changement de personnalité du bot.")
+  .addStringOption((option) =>
+    option
+      .setName("personality")
+      .setDescription("Nom de la personnalité à utiliser.")
+.addChoices(...commandChoices())  )
+
  const action = (message) => {
    const args = message.content.tolowercase().split(" ");
-   const namelist = Object.keys(personalities); // list of all personalities names
+   const nameList = getPersonalityNames();
    const replies = personality.getcommands().personality;
 
    if (args.length === 1) {
      // if no content, send actual personality name
      message.reply(replies.currentname + personality.getname() + ".");
    } else if (args[1]) {
-     if (namelist.includes(args[1])) {
+     if (nameList.includes(args[1])) {
        // if args[1] is in personalities.json
        const foundpersonality = Object.values(personalities).find(
          (obj) => obj.name === args[1]
@@ -22,13 +48,14 @@ const personalities = JSON.parse(readFileSync("static/personalities.json"));
        }
      } else if (args[1] === "list") {
        // send  personality name list
-       message.reply(replies.namelist + `${namelist.join(", ")}.`);
+       message.reply(replies.namelist + `${nameList.join(", ")}.`);
      } else message.reply(replies.nameerror);
    }
  };
 
  const personality = {
    name: "personality",
+   command,
    action,
    help: () => {
      return personality.getcommands().personality.help;
