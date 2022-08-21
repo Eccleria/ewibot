5b65307734b0364b677bb7e3df7bd39b71ab69ff
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-08-21 14:57:28 +0200

    [PRETTIER]

--- src/commands/personality.js
+++ src/commands/personality.js
@@ -11,7 +11,7 @@ const getPersonalityNames = () => {
   const nameList = Object.keys(personalities); // list of all personalities names
   const filtered = nameList.filter((str) => str !== "admin");
   return filtered;
-}
+};
 
 /** Create the choices of the personality command.
  * @returns {object[]}
@@ -23,51 +23,51 @@ const commandChoices = () => {
   }, []);
 
   return result;
-}
+};
 
 const command = new SlashCommandBuilder()
   .setName("personality")
-  .setDescription("Changement de personnalité du bot.")
+  .setDescription("Changement de personnalitÃ© du bot.")
   .addStringOption((option) =>
     option
       .setName("personality")
-      .setDescription("Nom de la personnalité à utiliser.")
+      .setDescription("Nom de la personnalitÃ© Ã  utiliser.")
       .addChoices(...commandChoices())
   );
 
- const action = (message) => {
-   const args = message.content.tolowercase().split(" ");
-   const nameList = getPersonalityNames();
-   const replies = personality.getcommands().personality;
+const action = (message) => {
+  const args = message.content.tolowercase().split(" ");
+  const nameList = getPersonalityNames();
+  const replies = personality.getcommands().personality;
 
-   if (args.length === 1) {
-     // if no content, send actual personality name
-     message.reply(replies.currentname + personality.getname() + ".");
-   } else if (args[1]) {
-     if (nameList.includes(args[1])) {
-       // if args[1] is in personalities.json
-       const foundpersonality = Object.values(personalities).find(
-         (obj) => obj.name === args[1]
-       );
-       if (foundpersonality) {
-         personality.set(foundpersonality.name, foundpersonality);
-         message.reply(replies.change + `${args[1]}.`);
-       }
-     } else if (args[1] === "list") {
-       // send  personality name list
-       message.reply(replies.namelist + `${nameList.join(", ")}.`);
-     } else message.reply(replies.nameerror);
-   }
- };
+  if (args.length === 1) {
+    // if no content, send actual personality name
+    message.reply(replies.currentname + personality.getname() + ".");
+  } else if (args[1]) {
+    if (nameList.includes(args[1])) {
+      // if args[1] is in personalities.json
+      const foundpersonality = Object.values(personalities).find(
+        (obj) => obj.name === args[1]
+      );
+      if (foundpersonality) {
+        personality.set(foundpersonality.name, foundpersonality);
+        message.reply(replies.change + `${args[1]}.`);
+      }
+    } else if (args[1] === "list") {
+      // send  personality name list
+      message.reply(replies.namelist + `${nameList.join(", ")}.`);
+    } else message.reply(replies.nameerror);
+  }
+};
 
- const personality = {
-   name: "personality",
-   command,
-   action,
-   help: () => {
-     return personality.getcommands().personality.help;
-   },
-   admin: true,
- };
+const personality = {
+  name: "personality",
+  command,
+  action,
+  help: () => {
+    return personality.getcommands().personality.help;
+  },
+  admin: true,
+};
 
- export default personality;
+export default personality;
