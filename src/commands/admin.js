47993f3ed39ebdb44daf1983b16cd51f1f224b9a
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-09-07 18:23:57 +0200

    [NEW] add saveLog admin command

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -1,49 +1,47 @@
 import { ContextMenuCommandBuilder } from "@discordjs/builders";
 
-import { getLogChannel } from "../admin/utils.js";
+import { getLogChannel, gifRecovery } from "../admin/utils.js";
 import { interactionReply } from "./utils.js";
-import { getAdminLogs } from "../helpers/index.js";
 import { PERSONALITY } from "../personality.js";
 
 const command = new ContextMenuCommandBuilder()
   .setName("save-log")
-  .setType(3);
+  .setType(3)
+  .setDefaultMemberPermissions(0x0000000000000020); //MANAGE_GUILD bitwise
 
 const action = async (interaction) => {
   console.log("action");
 
-  interactionReply(interaction, personality.sending);  //reply to interaction
-
   const message = interaction.targetMessage; //get message
   const embeds = message.embeds; //get embeds
 
-  const personality = PERSONALITY.getCommands().saveLog; //get personality
+  const personality = PERSONALITY.getCommands(); //get personality
+  const admin = PERSONALITY.getAdmin();
+  const saveLogP = personality.saveLog;
   const logChannel = await getLogChannel(interaction); //get logChannel
+
+  interactionReply(interaction, saveLogP.sending);  //reply to interaction
 
   if (embeds) {
     //add executor of saveLog
     const member = interaction.member;
+    console.log(embeds[0].fields)
     embeds[0].addFields(
       { name: personality.author, value: member.toString(), inline: true }
     );
 
     logChannel.send({ embeds: embeds, allowed_mentions: { parse: [] } }); //Send log
 
-    const dbData = getAdminLogs(interaction.client.db);
-    const logIds = dbData.reduce((acc, cur) => {
-      return [...acc, ...cur];
-    }, []); //regroup all ids
+    const test = admin.messageDelete.text; //get messageDelete text field name
+    const fields = embeds[0].fields; //get embed fields
+    const field = fields.filter((obj) => obj.name === test); //get corresponding field
 
-    const max = logIds.length - 1;
-    const idx = logIds.findIndex((id) => id === message.id);
-    //-1 or length => return
-    if (idx > -1 && idx < max) {
-      const id = logIds[idx + 1];
-      const threadChannel = await getLogChannel(interaction, "thread");
-      const gif = await threadChannel.messages.fetch(id);
-      logChannel.send({ content: gif.content })
+    const gifs = field.length !== 0 ? gifRecovery(field.value) : null; //if any field, find gifs
+    if (gifs !== null) {
+      const content = gifs.join("\n");
+      logChannel.send(content);
     }
-  }
+  } else logChannel.send(message.content);
 };
 
 const saveLog = {
