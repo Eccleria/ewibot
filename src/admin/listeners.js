01bb693dafa6e1d11634de2e5af3a6d1d1e51691
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-09-08 11:50:27 +0200

    [MERGE] fix/admin_logs 3

--- src/admin/listeners.js
+++ src/admin/listeners.js
@@ -12,6 +12,7 @@ import {
   getLogChannel,
   gifRecovery,
   setupEmbed,
+  sliceAddString,
 } from "./utils.js";
 import {
   addAdminLogs,
@@ -416,9 +417,22 @@ export const onMessageDelete = async (message) => {
     [embed]
   );
 
-  //handle gifs
+  //handle content
   let content = message.content ? message.content : messageDel.note;
-  const gifs = gifRecovery(content);
+  const len = content.length; //get content length
+  const slice = Math.ceil(len / 1024); //get number of time to slice content by 1024;
+
+  if (slice > 1) {
+    const sliced = sliceAddString(slice, content); //slice and add to embed
+
+    sliced.forEach((str, idx) => {
+      if (idx === 0)
+        embed.addFields({ name: messageDel.text, value: str }); //name's different from others
+      else embed.addFields({ name: messageDel.textAgain, value: str });
+    });
+  } else embed.addFields({ name: messageDel.text, value: content });
+
+  const gifs = gifRecovery(content); //handle gifs
 
   //if no AuditLog
   if (!deletionLog) {
@@ -427,7 +441,7 @@ export const onMessageDelete = async (message) => {
       auditLog.noLog,
       embeds,
       logChannel,
-      content,
+      null,
       attachments
     );
     if (gifs !== null) {
@@ -452,7 +466,7 @@ export const onMessageDelete = async (message) => {
       executor.tag,
       embeds,
       logChannel,
-      content,
+      null,
       attachments
     );
     if (gifs !== null) {
@@ -470,7 +484,7 @@ export const onMessageDelete = async (message) => {
       auditLog.noExec,
       embeds,
       logChannel,
-      content,
+      null,
       attachments
     );
     if (gifs !== null) {
@@ -575,13 +589,39 @@ export const onMessageUpdate = async (oldMessage, newMessage) => {
   //filter changes, if < 2 length => return
   const isLengthy = Math.abs(oldContent.length - newContent.length) >= 2;
   if (oldContent !== newContent && isLengthy) {
-    const oLen = oldContent.length !== 0;
-    const nLen = newContent.length !== 0;
+    const oLen = oldContent.length;
+    const nLen = newContent.length;
 
-    if (oLen) embed.addField(messageU.contentOld, oldContent); //to not add empty strings
-    if (nLen) embed.addField(messageU.contentNew, newContent);
+    if (oLen !== 0) {
+      const oSlice = Math.ceil(oLen / 1024); //get number of time to slice oldContent by 1024;
 
-    if (oLen && nLen) {
+      if (oSlice > 1) {
+        const oSliced = sliceAddString(oSlice, oldContent); //slice and add to embed
+
+        oSliced.forEach((str, idx) => {
+          console.log(idx, str)
+          console.log(messageU.contentOld)
+          if (idx === 0)
+            embed.addFields({ name: messageU.contentOld, value: str }); //name's different from others
+          else embed.addFields({ name: messageU.contentOldAgain, value: str });
+        });
+      } else embed.addFields({ name: messageU.contentOld, value: oldContent });
+
+    }
+    if (nLen !== 0) {
+      const nSlice = Math.ceil(nLen / 1024); //get number of time to slice oldContent by 1024;
+      if (nSlice > 1) {
+        const nSliced = sliceAddString(nSlice, newContent); //slice and add to embed
+
+        nSliced.forEach((str, idx) => {
+          if (idx === 0)
+            embed.addFields({ name: messageU.contentNew, value: str }); //name's different from others
+          else embed.addFields({ name: messageU.contentNewAgain, value: str });
+        });
+      }
+    } else embed.addFields({ name: messageU.contentNew, value: newContent });
+
+    if (oLen !== 0 && nLen !== 0) {
       //check for apology
       const oSanitized = sanitizePunctuation(oldContent.toLowerCase()); //remove punctuation
       const nSanitized = sanitizePunctuation(newContent.toLowerCase());
