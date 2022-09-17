28c614b10c4add5fe4a79548cceb661be0f062cb
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-09-17 23:41:45 +0200

    [REMOVE] apology dedicated dbHelper functions

--- src/helpers/dbHelper.js
+++ src/helpers/dbHelper.js
@@ -26,46 +26,6 @@ const removeAdminLogs = (db, type) => {
 
 export { addAdminLogs, getAdminLogs, removeAdminLogs };
 
-// APOLOGY COUNTING
-const isApologyUser = (authorId, db) => {
-  return db.data.apologiesCounting
-    .map((obj) => {
-      return obj.userId;
-    })
-    .includes(authorId);
-};
-
-const addApologyCount = (authorId, db) => {
-  const { apologiesCounting } = db.data;
-
-  if (isApologyUser(authorId, db)) {
-    // If already in DB, add +1 to the counter
-    for (const obj of apologiesCounting) {
-      if (obj.userId === authorId) {
-        obj.counter++;
-      }
-    }
-  } else {
-    // Else add user
-    db.data.apologiesCounting = [
-      ...db.data.apologiesCounting,
-      { userId: authorId, counter: 1 },
-    ];
-  }
-  db.wasUpdated = true;
-};
-
-const removeAppologyCount = (authorId, db) => {
-  if (isApologyUser(authorId, db)) {
-    db.data.apologiesCounting = db.data.apologiesCounting.filter(
-      (cur) => cur.userId !== authorId
-    );
-    db.wasUpdated = true;
-  }
-};
-
-export { isApologyUser, addApologyCount, removeAppologyCount };
-
 // BIRTHDAY
 const isBirthdayDate = (authorId, db) => {
   return db.data.birthdaysUsers
