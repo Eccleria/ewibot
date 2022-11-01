3f1799e5392ac00d822075e251c92519e86a6d75
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-11-01 18:10:46 +0100

    [NEW] add corresponding command declaration

--- src/commands/gift.js
+++ src/commands/gift.js
@@ -6,18 +6,30 @@ import { PERSONALITY } from "../personality.js";
 const command = new SlashCommandBuilder()
     .setName(PERSONALITY.getCommands().gift.name)
     .setDescription(PERSONALITY.getCommands().gift.description)
-    .addUserOption((option) => 
-        option
-            .setName(PERSONALITY.getCommands().gift.userOption.name)
-            .setDescription(PERSONALITY.getCommands().gift.userOption.description)
-            .setRequired(true)
+    .addSubcommand((subcommand) => //user authorisation command
+        subcommand
+            .setName(PERSONALITY.getCommands().gift.user.name)
+            .setDescription(PERSONALITY.getCommands().gift.user.description)
     )
-    .addStringOption((option) =>
-        option
-            .setName(PERSONALITY.getCommands().gift.textOption.name)
-            .setDescription(PERSONALITY.getCommands().gift.textOption.description)
-            .setRequired(true)
+    .addSubcommand((subcommand) => //send message command
+        subcommand
+            .setName(PERSONALITY.getCommands().gift.send.name)
+            .setDescription(PERSONALITY.getCommands().gift.send.description)
+            .addUserOption((option) =>
+                option
+                    .setName(PERSONALITY.getCommands().gift.userOption.name)
+                    .setDescription(PERSONALITY.getCommands().gift.send.userOption.description)
+                    .setRequired(true)
+            )
+            .addStringOption((option) =>
+                option
+                    .setName(PERSONALITY.getCommands().gift.textOption.name)
+                    .setDescription(PERSONALITY.getCommands().gift.send.textOption.description)
+                    .setRequired(true)
+            )
     )
+
+
 
 const action = async (interaction) => {
 
