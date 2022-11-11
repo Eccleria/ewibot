3a00d89f48aa93acf87ac2405910414de5f37d57
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-11-11 18:13:18 +0100

    [NEW] add stringOption choices

--- src/commands/announce.js
+++ src/commands/announce.js
@@ -1,8 +1,26 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 
-import { PERSONALITY } from "../personality"
-import { interactionReply } from "./utils"
+import { PERSONALITY } from "../personality";
+import { interactionReply } from "./utils";
 
+const giftAnnounce = {
+  name: "gift",
+  value: "announce_gift"
+};
+
+const announceOptions = [giftAnnounce];
+
+const command = new SlashCommandBuilder()
+  .setName(PERSONALITY.getCommands().announce.name)
+  .setDescription(PERSONALITY.getCommands().announce.description)
+  .setDefaultMemberPermissions(0x0000010000000000)
+  .addStringOption(
+    (option) =>
+      option
+        .setName(PERSONALITY.getCommands().announce.stringOption.name)
+        .setDescription(PERSONALITY.getCommands().announce.stringOption.description)
+        .addChoices(...announceOptions)
+  );
 
 const announce = {
   action,
