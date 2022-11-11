b0925a5c4cfda4df9b07b65fd7a5c8a2cb67b888
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-11-11 18:39:54 +0100

    [NEW] add basic command action

--- src/commands/announce.js
+++ src/commands/announce.js
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { isAdmin } from "../helpers";
 
 import { PERSONALITY } from "../personality";
 import { interactionReply } from "./utils";
@@ -6,6 +7,25 @@ import { interactionReply } from "./utils";
 const giftAnnounce = {
   name: "gift",
   value: "announce_gift"
+};
+
+const action = (interaction) => {
+  // handle announce command interaction
+
+  if (!isAdmin(interaction.author.id)) {
+    //check for bot admin
+    interactionReply(interaction, PERSONALITY.getCommands().announce.notAdmin);
+    return
+  }
+
+  const personality = PERSONALITY.getCommands().announce;
+
+  //get interaction data
+  //const client = interaction.client;
+  const options = interaction.options;
+  const whichAnnounce = options.getString(personality.stringOption.name);
+
+  interaction.reply(personality[whichAnnounce].confirm)
 };
 
 const announceOptions = [giftAnnounce];
