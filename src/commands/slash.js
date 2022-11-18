e0c9dbdda895a674b55164bf12f935f41118bba1
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-11-18 13:18:33 +0100

    [NEW] add help command basic definition

--- src/commands/slash.js
+++ src/commands/slash.js
@@ -1,9 +1,11 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-//import { SlashCommandBuilder } from "@discordjs/builders";
+import { SlashCommandBuilder } from "@discordjs/builders";
 
 import twitter from "./twitter.js";
 import saveLog from "./save-log.js";
+import { PERSONALITY } from "../personality.js";
+import { interactionReply } from "./utils.js";
 
 const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
@@ -20,11 +22,46 @@ const slashCommands = [
   twitter,
 ]; //command + action
 
+// HELP
+
 const helpCommands = [...contextCommands, ...slashCommands];
 /*const helpOptions = helpCommands.reduce((acc, cur) => {
   const cmd = cur.command;
   return [...acc, { name: cmd.name, value: cmd.name }];
 }, []);*/
+
+const help = {
+  action: (interaction) => {
+    const perso = PERSONALITY.getCommands();
+
+    const userOption = interaction.options.getString(perso.help.stringOption.name); //get option given by user
+    const foundCommand = helpCommands.find(
+      (cmd) => cmd.command.name === userOption
+    ); //find associated command
+
+    if (foundCommand) foundCommand.help(interaction); //execute foundCommand help()
+    else interactionReply(interaction, perso.help.notFound);
+  },
+  command: new SlashCommandBuilder()
+    .setName(PERSONALITY.getCommands().help.name)
+    .setDescription(PERSONALITY.getCommands().help.description)
+    .addStringOption((option) =>
+      option
+        .setName(PERSONALITY.getCommands().help.stringOption.name)
+        .setDescription(PERSONALITY.getCommands().help.stringOption.description)
+        .setRequired(true)
+        .setAutocomplete(true)
+  ),
+  help: (interaction) => {
+    const personality = PERSONALITY.getCommands().help.help;
+    interactionReply(interaction, personality);
+  }
+}
+
+helpCommands.push(help);
+slashCommands.push(help);
+
+// API
 
 export const slashCommandsInit = async (self, guildId, client) => {
   try {
@@ -32,7 +69,7 @@ export const slashCommandsInit = async (self, guildId, client) => {
 
     await rest.put(Routes.applicationGuildCommands(self, guildId), {
       body: helpCommands.map((cmd) => cmd.command.toJSON()),
-    });
+    }); //send commands jsons to API for command create/update
 
     console.log("Successfully reloaded application (/) commands.");
 
