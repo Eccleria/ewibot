ca3ca20ebdbc37b0dc99bf8d6c345cc33deb001f
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-11-19 16:32:28 +0100

    [FIX] double interactionReply import in slash - issue from merge

--- src/commands/slash.js
+++ src/commands/slash.js
@@ -15,7 +15,6 @@ import {
   //utils
   //isAdmin,
 } from "../helpers/index.js";
-import { interactionReply } from "./utils.js";
 
 import announce from "./announce.js";
 import birthday from "./birthday.js";
