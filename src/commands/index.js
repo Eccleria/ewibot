e7a26852a78c11a5ec7a096033d515b6bb219edb
Eccleria <97692338+Eccleria@users.noreply.github.com>
2022-12-16 10:21:18 +0100

    [REMOVE] $spotify

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,23 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import reminder from "./reminder.js";
-import spotify from "./spotify.js";
 import leaderboardApology from "./leadApo.js";
 import pronouns from "./pronouns.js";
 
-const commands =
-  process.env.USE_SPOTIFY === "yes"
-    ? [
+const commands =[
         leaderboardApology,
         pronouns,
-        reminder,
-        spotify,
-      ]
-    : [
-        leaderboardApology,
-        pronouns,
-        reminder,
       ];
 
 export default commands; // Regroups all commands
