2701e27663a2ae55632fb34893a711ab05a89304
Eccléria <97692338+Eccleria@users.noreply.github.com>
2023-06-22 21:29:55 +0200

    [LINT] remove Event test

--- src/bot.js
+++ src/bot.js
@@ -7,7 +7,7 @@ import "dayjs/locale/fr.js";
 dayjs.extend(RelativeTime);
 dayjs.locale("fr");
 
-import { ChannelType, Client, GatewayIntentBits, Partials, Events } from "discord.js";
+import { ChannelType, Client, GatewayIntentBits, Partials } from "discord.js";
 import SpotifyWebApi from "spotify-web-api-node";
 
 import { roleInit } from "./admin/role.js";
