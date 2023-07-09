257198a915d7794daa8265953c740d97600b1d1d
Eccléria <97692338+Eccleria@users.noreply.github.com>
2023-07-09 15:33:40 +0200

    [DRAFT] test for dessinateurices

---
+++ src/commands/test-dessin.js
@@ -0,0 +1,72 @@
+import Canvas from "canvas";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { PERSONALITY } from "../personality";
+
+const maxLength = 18;
+const sizeX = 457;
+const sizeY = 640;
+
+const getCircleValues = (userId, circle, canExceed) => {
+  const idx = (circle - 1) * maxLength;
+  let value;
+
+  //radius
+  value = Number(userId.slice(idx, idx + 2));
+  const radius = (canExceed ? value * sizeX : value * sizeY) / 99 / 2;
+
+  //posX
+  value = Number(userId.slice(idx + 2, idx + 4));
+  const posX =
+    (canExceed ? value * (sizeY - 2 * radius) : value * (sizeX - 2 * radius)) /
+      99 +
+    radius;
+
+  //posY
+  value = Number(userId.slice(idx + 4, idx + 6));
+  const posY = canExceed ? value * sizeX : value * sizeY;
+
+  return [radius, posX, posY];
+};
+
+const command = new SlashCommandBuilder()
+  .setName(PERSONALITY.getCommands().testdessin.name)
+  .setDescription(PERSONALITY.getCommands().testdessin.description)
+  .setDefaultMemberPermissions(0x0000010000000000); //only moderation staff
+
+const action = (interaction) => {
+  //get user information
+  const userId = interaction.member.id;
+
+  //compute test values
+  const len = userId.length;
+  const id =
+    len >= maxLength
+      ? userId.slice(0, maxLength)
+      : userId + userId.slice(0, maxLength - len);
+  const canExceed = Number(id[0]) % 2;
+
+  //compute circles position/sizes
+  const posSizes = getCircleValues(id);
+
+  //build test image
+  const canvas = Canvas.createCanvas(sizeX, sizeY);
+  const context = canvas.getContext("2d");
+
+  context.arc();
+
+  //send test image
+};
+
+const testdessin = {
+  action,
+  command,
+  help: (interaction) => {
+    const personality = PERSONALITY.getCommands().testdessin;
+    interactionReply(interaction, personality.help);
+  },
+  admin: true,
+  releaseDate: null,
+  sentinelle: true,
+};
+
+export default testdessin;
