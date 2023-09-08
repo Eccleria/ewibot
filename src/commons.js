84edba3b378db5762ee62620e2133a5a9b36c2e2
Eccléria <97692338+Eccleria@users.noreply.github.com>
2023-09-08 16:45:49 +0200

    [DOC] add docstrings to Commons Class

--- src/commons.js
+++ src/commons.js
@@ -2,6 +2,9 @@
 import { readFileSync } from "fs";
 const commons = JSON.parse(readFileSync("static/commons.json"));
 
+/**
+ * Class giving access to commons.json data for all bot files
+ */
 class Commons {
   constructor(test, prod, shared) {
     this.test = test;
@@ -9,21 +12,46 @@ class Commons {
     this.shared = shared;
   }
 
+  /**
+   * get Test server values
+   * @returns {object} specific values for test server
+   */
   getTest() {
     return this.test;
   }
+
+  /**
+   * get Prod server values
+   * @returns {object} specific values for prod server
+   */
   getProd() {
     return this.prod;
   }
+
+  /**
+   * get Shared values for both servers
+   * @returns {object} values shared for both servers
+  */
   getShared() {
     return this.shared;
   }
+
+  /**
+   * get correct commons.json data, according to given guildId
+   * @param {string} guildId Guild id
+   * @returns {object} specific values for prod or test server
+   */
   fetchFromGuildId(guildId) {
     return this.list.find((obj) => guildId === obj.guildId);
   }
+  /**
+   * get both test & prod values in a list
+   * @returns {object} Both test, prod object in a list
+   */
   getList() {
     return [this.test, this.prod];
   }
 }
 
+//Unique instance of Commons class, used in all files
 export const COMMONS = new Commons(commons.test, commons.prod, commons.shared);
