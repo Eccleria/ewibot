1c6f856a50a7cd00db72f8aa94beae8070df5618
Eccléria <97692338+Eccleria@users.noreply.github.com>
2023-10-21 17:13:19 +0200

    [PRETTIER]

--- src/commands/vest.js
+++ src/commands/vest.js
@@ -7,104 +7,109 @@ import fs from "fs";
 import { interactionEditReply } from "./polls/pollsUtils.js";
 
 const command = new SlashCommandBuilder()
-    .setName(PERSONALITY.getCommands().vest.name)
-    .setDescription(PERSONALITY.getCommands().vest.description)
-    .addUserOption((option) => 
-      option
-        .setName(PERSONALITY.getCommands().vest.userOption.name)
-        .setDescription(PERSONALITY.getCommands().vest.userOption.description)
-        .setRequired(true)
-    )
-    .addBooleanOption((option) => 
-      option
-        .setName(PERSONALITY.getCommands().vest.forceOption.name)
-        .setDescription(PERSONALITY.getCommands().vest.forceOption.description)
-        .setRequired(false)
-    );
+  .setName(PERSONALITY.getCommands().vest.name)
+  .setDescription(PERSONALITY.getCommands().vest.description)
+  .addUserOption((option) =>
+    option
+      .setName(PERSONALITY.getCommands().vest.userOption.name)
+      .setDescription(PERSONALITY.getCommands().vest.userOption.description)
+      .setRequired(true)
+  )
+  .addBooleanOption((option) =>
+    option
+      .setName(PERSONALITY.getCommands().vest.forceOption.name)
+      .setDescription(PERSONALITY.getCommands().vest.forceOption.description)
+      .setRequired(false)
+  );
 
 const action = async (interaction) => {
-    const perso = PERSONALITY.getCommands().vest;
-    const {guild, options} = interaction;
-    
-    await interaction.deferReply();
-    const user = options.getUser(perso.userOption.name);
-    const target = await guild.members.fetch(user.id); //get guildMember from user id
+  const perso = PERSONALITY.getCommands().vest;
+  const { guild, options } = interaction;
 
-    //get pp hash
-    const url = target.displayAvatarURL({ extension: "png" });
-    const urlHash = url.split(`${target.id}/`)[1].split('.')[0];
-    const fileName = `${target.id}-${urlHash}.png`;
-    console.log("target image url", [url], [urlHash]);
+  await interaction.deferReply();
+  const user = options.getUser(perso.userOption.name);
+  const target = await guild.members.fetch(user.id); //get guildMember from user id
 
-    //check if older image exists
-    const pngsPath = path.join(
-        path.resolve(path.dirname("")),
-        "pics",
-        "vest",
-        "pngs"
+  //get pp hash
+  const url = target.displayAvatarURL({ extension: "png" });
+  const urlHash = url.split(`${target.id}/`)[1].split(".")[0];
+  const fileName = `${target.id}-${urlHash}.png`;
+  console.log("target image url", [url], [urlHash]);
+
+  //check if older image exists
+  const pngsPath = path.join(
+    path.resolve(path.dirname("")),
+    "pics",
+    "vest",
+    "pngs"
+  );
+  const dir = fs.readdirSync(pngsPath);
+  const gifExists = dir.reduce((acc, cur) => {
+    if (cur.includes(fileName)) return true;
+    else if (cur.startsWith(target.id)) {
+      fs.unlinkSync(`${pngsPath}/${cur}`);
+      return false;
+    }
+    return acc;
+  }, false);
+
+  //get force option if any
+  const option = options.getBoolean(perso.forceOption.name);
+  const force = option ? option : false;
+
+  //build image
+  if (!gifExists || force) {
+    const canvas = Canvas.createCanvas(1078, 1260); // Canvas creation
+    const context = canvas.getContext("2d"); // context allows canvas further modification
+    const avatar = await Canvas.loadImage(
+      // Load target avatar
+      target.displayAvatarURL({ extension: "png" })
     );
-    const dir = fs.readdirSync(pngsPath);
-    const gifExists = dir.reduce((acc, cur) => {
-        if (cur.includes(fileName)) return true;
-        else if (cur.startsWith(target.id)) {
-            fs.unlinkSync(`${pngsPath}/${cur}`);
-            return false;
-        }
-        return acc;
-    }, false);
+    //add background to canvas
+    const basicPath = path.join(path.resolve(path.dirname("")), "pics", "vest");
+    const picture = await Canvas.loadImage(
+      `${basicPath}/Gilet pare-balles perdues.png`
+    );
+    context.drawImage(picture, 0, 0, canvas.width, canvas.height); //add background
+    context.save(); //Save the general configuration
 
-    //get force option if any
-    const option = options.getBoolean(perso.forceOption.name);
-    const force = option ? option : false;
+    //draw circle
+    context.beginPath(); //Pick up the pen
+    context.arc(550, 440, 80, 0, Math.PI * 2, true); //Start the arc to form a circle
+    context.closePath(); //Put the pen down
+    context.clip(); //Clip off the region you drew on
 
-    //build image
-    if (!gifExists || force) {
-        const canvas = Canvas.createCanvas(1078, 1260); // Canvas creation
-        const context = canvas.getContext("2d"); // context allows canvas further modification
-        const avatar = await Canvas.loadImage(
-            // Load target avatar
-            target.displayAvatarURL({ extension: "png" })
-        );
-        //add background to canvas
-        const basicPath = path.join(
-            path.resolve(path.dirname("")),
-            "pics",
-            "vest"
-        );
-        const picture = await Canvas.loadImage(`${basicPath}/Gilet pare-balles perdues.png`);
-        context.drawImage(picture, 0, 0, canvas.width, canvas.height); //add background
-        context.save(); //Save the general configuration
+    //add pp
+    context.drawImage(avatar, 470, 360, 160, 160);
+    context.restore(); //Go back to the general contribution
 
-        //draw circle
-        context.beginPath(); //Pick up the pen
-        context.arc(550, 440, 80, 0, Math.PI * 2, true); //Start the arc to form a circle
-        context.closePath(); //Put the pen down
-        context.clip(); //Clip off the region you drew on
+    //add nickname
+    const filtered = target.nickname
+      ? removeEmote(target.nickname)
+      : removeEmote(target.displayName);
+    const text = filtered.length > 12 ? filtered.slice(0, 12) : filtered;
 
-        //add pp
-        context.drawImage(avatar, 470, 360, 160, 160);
-        context.restore(); //Go back to the general contribution
+    context.font = applyText(canvas, text);
+    context.fillStyle = "#000000";
+    context.translate(712, 490);
+    context.rotate(Math.PI / 20);
+    context.fillText(text, 0, 0);
 
-        //add nickname
-        const filtered = target.nickname ? removeEmote(target.nickname) : removeEmote(target.displayName);
-        const text = filtered.length > 12 ? filtered.slice(0, 12) : filtered;
-
-        context.font = applyText(canvas, text);
-        context.fillStyle = "#000000";
-        context.translate(712, 490);
-        context.rotate(Math.PI / 20);
-        context.fillText(text, 0, 0);
-
-        const buffer = canvas.toBuffer("image/png");
-        fs.writeFileSync(`${pngsPath}/${fileName}`, buffer); //Write the gif locally
-        const attachment = new AttachmentBuilder(buffer, {name: "test.png"});
-        interactionEditReply(interaction, {content: perso.sent, files: [attachment]});
-    }
-    else {
-        const buffer = fs.readFileSync(`${pngsPath}/${fileName}`);
-        const attachment = new AttachmentBuilder(buffer, {name: "test.png"});
-        interactionEditReply(interaction, {content: perso.sent, files: [attachment]});
-    }
+    const buffer = canvas.toBuffer("image/png");
+    fs.writeFileSync(`${pngsPath}/${fileName}`, buffer); //Write the gif locally
+    const attachment = new AttachmentBuilder(buffer, { name: "test.png" });
+    interactionEditReply(interaction, {
+      content: perso.sent,
+      files: [attachment],
+    });
+  } else {
+    const buffer = fs.readFileSync(`${pngsPath}/${fileName}`);
+    const attachment = new AttachmentBuilder(buffer, { name: "test.png" });
+    interactionEditReply(interaction, {
+      content: perso.sent,
+      files: [attachment],
+    });
+  }
 };
 
 /**
@@ -114,28 +119,28 @@ const action = async (interaction) => {
  * @returns font with correct fontSize
  */
 const applyText = (canvas, text) => {
-	const context = canvas.getContext('2d');
+  const context = canvas.getContext("2d");
 
-    let fontSize = 60;
-	do {
-		// Assign the font to the context and decrement it so it can be measured again
-		context.font = `${fontSize -= 3}px sans-serif`;
-		// Compare pixel width of the text to the canvas minus the approximate avatar size
-	} while (context.measureText(text).width > 190);
+  let fontSize = 60;
+  do {
+    // Assign the font to the context and decrement it so it can be measured again
+    context.font = `${(fontSize -= 3)}px sans-serif`;
+    // Compare pixel width of the text to the canvas minus the approximate avatar size
+  } while (context.measureText(text).width > 190);
 
-	return context.font;
+  return context.font;
 };
 
 const vest = {
-    command,
-    action,
-    help: (interaction) => {
-        const perso = PERSONALITY.getCommands().vest;
-        interactionReply(interaction, perso.help);
-    },
-    admin: false,
-    releaseDate: null,
-    sentinelle: false
+  command,
+  action,
+  help: (interaction) => {
+    const perso = PERSONALITY.getCommands().vest;
+    interactionReply(interaction, perso.help);
+  },
+  admin: false,
+  releaseDate: null,
+  sentinelle: false,
 };
-    
+
 export default vest;
