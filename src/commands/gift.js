0adb82aeb740c2bb48c5671a43e9e9bcdedd3100
Eccléria <97692338+Eccleria@users.noreply.github.com>
2023-12-25 01:04:06 +0100

    [FIX] wrong color + update timeout value for correction

--- src/commands/gift.js
+++ src/commands/gift.js
@@ -82,7 +82,7 @@ const giftInteractionCreation = async (client, type) => {
   if (type === "xmas") {
     const nDayEmbed = personality.nDayEmbed;
     const embed = new EmbedBuilder() //create embed
-      .setColor("DARK_GREEN")
+      .setColor("DarkGreen")
       .setTimestamp()
       .setTitle(personality.nDayEmbed.title)
       .setDescription(nDayEmbed.description)
@@ -94,7 +94,7 @@ const giftInteractionCreation = async (client, type) => {
     const newYear = personality.newYear;
 
     const embed = new EmbedBuilder() //create embed
-      .setColor("DARK_GREEN")
+      .setColor("DarkGreen")
       .setTimestamp()
       .setTitle(newYear.title)
       .setDescription(newYear.description);
@@ -111,18 +111,18 @@ const addSeparationToDb = (client) => {
 
 export const setGiftTimeoutLoop = (client) => {
   // setup Timeout before n-Surprise day
-  const xmasDate = dayjs(new Date(2023, 11, 25, 1)); //xmas date when to send
+  const xmasDate = dayjs(new Date(2023, 11, 25, 1, 3)); //xmas date when to send
   const nyDate = dayjs(new Date(2024, 0, 1, 1)); //new year date
   const switchDate = dayjs(new Date(2023, 11, 27, 1)); //add separator to messages
 
-  const tomorrowMidnight = dayjs()
+  /*const tomorrowMidnight = dayjs()
     .add(1, "day")
     .hour(0)
     .minute(0)
     .second(0)
     .millisecond(0); //tomorrow @ midnight
-
-  const timeToMidnight = tomorrowMidnight.diff(dayjs());
+  */
+  const timeToMidnight = 1000;// tomorrowMidnight.diff(dayjs());
   const dayMs = 86400000;
 
   const sendMessage = () => {
