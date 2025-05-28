31c65ee3bfe855db40d870100c98374809f26674
Eccleria <eccleria@gmail.com>
2025-05-29 00:04:36 +0200

    [DRAFT] remove some json data

--- src/commands/announce.js
+++ src/commands/announce.js
@@ -1,55 +1,12 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import {
   ActionRowBuilder,
-  EmbedBuilder,
   ButtonStyle,
-  Colors,
   MessageFlags,
 } from "discord.js";
 import { createButton } from "./utils.js";
 import { interactionReply, isAdmin } from "../helpers/index.js";
-import { COMMONS } from "../commons.js";
 import { PERSONALITY } from "../personality.js";
-
-// GIFT Announce
-const giftAction = async (interaction) => {
-  //action to fire once correct button is clicked
-  const personality = PERSONALITY.getAnnounces().announce_gift;
-  interactionReply(interaction, personality.sending);
-
-  //create announce
-  const fields = personality.fields;
-  const embed = new EmbedBuilder()
-    .setColor(Colors.DarkGreen)
-    .setTimestamp()
-    .setTitle(personality.title)
-    .setDescription(personality.description)
-    .setFooter({
-      text: personality.footer,
-      iconURL:
-        "https://cdn.discordapp.com/avatars/691336942117453876/6d73900209e4d3bc35039f68f4aa9789.webp",
-    })
-    .addFields(Object.values(fields))
-    .setThumbnail(
-      "https://media.discordapp.net/attachments/959815577575256124/1041070360461852724/Ewilan_writing_cut.png?width=670&height=670",
-    );
-
-  //get channel
-  const server = COMMONS.fetchFromGuildId(interaction.guildId);
-  const channelId = server.announce.giftChannelId;
-  const channel = await interaction.client.channels.fetch(channelId);
-
-  //send gift announce
-  channel.send({ embeds: [embed] });
-};
-
-const giftAnnounce = {
-  action: giftAction,
-  button: {
-    name: "gift",
-    value: "announce_gift",
-  },
-};
 
 // ANNOUNCE
 
@@ -83,7 +40,7 @@ const action = (interaction) => {
 };
 
 //list of announces
-const announces = [giftAnnounce]; //list of all announces
+const announces = []; //list of all announces
 
 //button action dispatcher
 export const announceButtonHandler = (interaction) => {
