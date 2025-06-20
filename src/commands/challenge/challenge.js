44efe01ea5cceeccaf2360538df2765ebcbe86ee
Eccléria <97692338+Eccleria@users.noreply.github.com>
2025-06-20 22:00:09 +0200

    [UPDATE] add hammertime container

--- src/commands/challenge/challenge.js
+++ src/commands/challenge/challenge.js
@@ -3,12 +3,13 @@ import { PERSONALITY } from "../../personality.js";
 
 import { interactionReply } from "../../helpers/index.js";
 import { createButton } from "../utils.js";
+import dayjs from "dayjs";
 
 export const createChallenge = async (interaction, challenge) => {
   const perso = PERSONALITY.getPersonality().challenge.challenge;
   const channel = await interaction.client.channels.fetch(interaction.channelId);
 
-  //build the challenge message
+  //build the first container for the challenge message
   //create basic objects
   const separator = new SeparatorBuilder()
     .setSpacing(SeparatorSpacingSize.Large);
@@ -29,7 +30,6 @@ export const createChallenge = async (interaction, challenge) => {
     .setContent(perso.author + interaction.user.toString());
   firstSection.addTextDisplayComponents(titleText, authorText);
 
-
   //add challenge description
   const button = createButton(perso.pButton.customId, perso.pButton.label, ButtonStyle.Success);
   secondSection.setButtonAccessory(button);
@@ -42,7 +42,16 @@ export const createChallenge = async (interaction, challenge) => {
   const settingButton = createButton(perso.sButton.customId, null, ButtonStyle.Secondary, "⚙️");
   const actionRow = new ActionRowBuilder().addComponents(settingButton);
 
-  
+  //build the second container for the challenge message
+  //compute timeout
+  const timeout = 24 * 60 * 60 * 1000; //1day in ms
+  const challengeDate = dayjs().millisecond(timeout);
+  const cDateUnix = challengeDate.unix();
+  const hammertimeText = `<t:${cDateUnix}:F> soit <t:${cDateUnix}:R>`;
+  const dateTextField = new TextDisplayBuilder()
+    .setContent(perso.hammertime + hammertimeText);
+
+
   //assemble the message
   const color = PERSONALITY.getColors().choices[18].value;
   const container = new ContainerBuilder()
@@ -52,8 +61,11 @@ export const createChallenge = async (interaction, challenge) => {
     .addSectionComponents(secondSection)
     .addSeparatorComponents(separator)
     .addActionRowComponents(actionRow);
-  
-  channel.send({allowed_mentions: { parse: [] }, flags: MessageFlags.IsComponentsV2, components: [container]});
+  const hammertimeContainer = new ContainerBuilder()
+    .setAccentColor(Colors[color])
+    .addTextDisplayComponents(dateTextField);
+
+  channel.send({allowed_mentions: { parse: [] }, flags: MessageFlags.IsComponentsV2, components: [container, hammertimeContainer]});
 };
 
 const command = new SlashCommandBuilder()
