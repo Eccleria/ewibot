e40d6e611281aa7a1ec06df824e52572c204a65a
Eccléria <97692338+Eccleria@users.noreply.github.com>
2025-08-08 19:32:36 +0200

    [NEW] add the challenge participate button logic

--- src/commands/challenge/challengeHandlers.js
+++ src/commands/challenge/challengeHandlers.js
@@ -1,12 +1,10 @@
 import { interactionReply } from "../../helpers/index.js";
-import { challengeParticipateButtonAction, challengeSendSettingsButtonAction } from "./challengeButton.js";
+import { challengeParticipateButtonAction } from "./challengeButton.js";
 
 export const challengeButtonHandler = (interaction) => {
   const { customId } = interaction;
 
   if (customId.includes("participate")) 
     challengeParticipateButtonAction(interaction);
-  else if (customId.includes("settings"))
-    challengeSendSettingsButtonAction(interaction); 
   else interactionReply(interaction, "ERROR 404");
 }
--- src/commands/challenge/challengeModal.js
+++ src/commands/challenge/challengeModal.js
@@ -1,3 +1,4 @@
+import { addChallengeParticipation } from "../../helpers/index.js";
 import { interactionReply } from "../../helpers/utils.js";
 import { PERSONALITY } from "../../personality.js";
 import { createChallenge } from "./challenge.js";
@@ -8,6 +9,8 @@ export const challengeModalHandler = (interaction) => {
 
   if (customId.includes("_userInput"))
     challengeUserInputModalHandler(interaction);
+  else if (customId.includes("_participate"))
+    challengeParticipateModalHandler(interaction);
 };
 
 const challengeUserInputModalHandler = (interaction) => {
@@ -23,3 +26,21 @@ const challengeUserInputModalHandler = (interaction) => {
   //reply to the modal
   interactionReply(interaction, perso.sent);
 };
+
+const challengeParticipateModalHandler = (interaction) => {
+  const perso = PERSONALITY.getPersonality().challenge.participate;
+
+  const text = interaction.fields.getTextInputValue(perso.textInput.customId);
+  const title = interaction.fields.getTextInputValue(perso.titleInput.customId);
+  console.log("challenge modal userInput text", [text], [title]);
+
+  //store the participation
+  addChallengeParticipation(interaction.client.db, interaction.message.id, interaction.user.id, text, title);
+
+  //update the participant count on the challenge message
+  const message = interaction.message;
+  console.log("participation message", message);
+
+  //reply to the modal
+  interactionReply(interaction, perso.sent);
+}
