2efe7cdc939e2f5fb4320ee901335ca3f1ca05c7
Eccleria <eccleria@gmail.com>
2025-09-10 00:43:04 +0200

    [LOGGER] now use ISO UTC date format

--- src/bot.js
+++ src/bot.js
@@ -62,6 +62,7 @@ export const logger = pino({
     target: "pino-pretty",
     options: {
       colorize: true,
+      translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'",
     },
   },
   redact: ["TOKEN"],
