10c6d9b2b70c86bd848cf88e8afafce7157be4d8
Eccleria <eccleria@gmail.com>
2025-09-10 18:43:35 +0200

    [DRAFT] logger children for each category

--- src/logger.js
+++ src/logger.js
@@ -18,7 +18,35 @@ export const logger = pino({
 //#endregion
 
 //#region Childs
+//Admin
+
+//commands
+export const birthdayLog = logger.child({module: 'birthday'});
+export const botEmoteLog = logger.child({module: 'botEmote'});
+export const botMessageLog = logger.child({module: 'botMessage'});
+export const concreteLog = logger.child({module: 'concrete'});
+export const eventRoleLog = logger.child({module: 'eventRole'});
+export const giftLog = logger.child({module: 'gift'});
+export const leadApoLog = logger.child({module: 'leadApo'});
+export const personalityLog = logger.child({module: 'personality'});
 export const pollLog = logger.child({module: 'polls'});
+export const pronounsLog = logger.child({module: 'pronouns'});
+export const reminderLog = logger.child({module: 'reminder'});
+export const reverseLog = logger.child({module: 'reverse'});
+export const shuttleLog = logger.child({module: 'shuttle'});
+export const slashLog = logger.child({module: 'slash'});
+export const statsLog = logger.child({module: 'stats'});
+export const commandUtilsLog = logger.child({module: 'commandUtils'});
+export const vestLog = logger.child({module: 'vest'});
+
+//helpers
+//db
+export const dbLog = logger.child({module: 'db'});
+
+//main
+export const funLog = logger.child({module: 'fun'});
+export const listenersLog = logger.child({module: 'mainListeners'});
+
 
 
 //#endregion
