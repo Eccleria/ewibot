452a7c4c115b9825a2bd5c36773de42bac615bbd
Eccleria <eccleria@gmail.com>
2025-11-09 18:35:23 +0100

    [NEW] add a wrapper for custom pino-prettier messages

--- src/listeners.js
+++ src/listeners.js
@@ -17,7 +17,7 @@ import { COMMONS } from "./classes/commons.js";
 import { readContentAndReact } from "./fun.js";
 import { emojiInContentHandler, statsGifCount } from "./stats.js";
 import { PERSONALITY } from "./classes/personality.js";
-import { listenersLog } from "./logger.js";
+import { listenersLog, reminderLog } from "./logger.js";
 
 //#region Listeners
 export const onInteractionCreate = (interaction) => {
@@ -191,14 +191,14 @@ export const onRemoveReminderReaction = (
           clearTimeout(timeout); //cancel timeout
           removeReminder(client.db, botMessage.id);
           botMessage.reply(PERSONALITY.getPersonality().reminder.delete);
-          listenersLog.info("reminder deleted");
+          reminderLog.info("reminder deleted");
           return false;
         }
         return true;
       });
       return;
     } catch (err) {
-      listenersLog.error(err, "reminderError");
+      reminderLog.error(err, "reminderError");
     }
   }
 };
