a4f983173431eae3368a769c156f964dbc09e6c4
Eccleria <eccleria@gmail.com>
2026-02-13 17:20:58 +0100

    [DRAFT] fetch Commons from ewilib + add fetchFromEnv

--- src/classes/commons.js
+++ src/classes/commons.js
@@ -1,56 +1,10 @@
 // jsons import
 import { readFileSync } from "fs";
+import { Commons } from "ewilib";
 const commons = JSON.parse(readFileSync("static/commons.json"));
 
-/**
- * Class giving access to commons.json data for all bot files
- */
-class Commons {
-  constructor(test, prod, shared) {
-    this._test = test;
-    this._prod = prod;
-    this._shared = shared;
-  }
-
-  /**
-   * get Test server values
-   * @returns {object} specific values for test server
-   */
-  getTest() {
-    return this._test;
-  }
-
-  /**
-   * get Prod server values
-   * @returns {object} specific values for prod server
-   */
-  getProd() {
-    return this._prod;
-  }
-
-  /**
-   * get Shared values for both servers
-   * @returns {object} values shared for both servers
-   */
-  getShared() {
-    return this._shared;
-  }
-
-  /**
-   * get correct commons.json data, according to given guildId
-   * @param {string} guildId Guild id
-   * @returns {object} specific values for prod or test server
-   */
-  fetchFromGuildId(guildId) {
-    return this.getList().find((obj) => guildId === obj.guildId);
-  }
-  /**
-   * get both test & prod values in a list
-   * @returns {object} Both test, prod object in a list
-   */
-  getList() {
-    return [this._test, this._prod];
-  }
+Commons.prototype.fetchFromEnv = function() {
+  return isProduction ? this._prod : this._test;
 }
 
 //Unique instance of Commons class, used in all files
