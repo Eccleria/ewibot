273a0ca186650f82e5a02e0509c759180f42082f
FocusedFaust <quieto297@gmail.com>
2026-05-20 20:12:10 +0200

    dbBlacklist: implement methods to manipulate blacklist in db

--- src/helpers/db/dbBlacklist.js
+++ src/helpers/db/dbBlacklist.js
@@ -1,11 +1,24 @@
 const addBlacklist = (db, userId) => {
-
+    var found = db.data.blacklistUsers.includes(userId);
+    if (!found){
+        db.data.blacklistUsers.push(userId);
+        db.wasUpdated = true;
+    }
+    return !found;
 }
 
 const removeBlacklist = (db, userId) => {
-
+    const found = db.data.blacklistUsers.includes(userId);
+    if (found) {
+        db.data.blacklistUsers = db.data.blacklistUsers.filter(
+            item => item !== userId);
+        db.wasUpdated = true;
+    }
+    return found;
 }
 
 const getBlacklist = (db) => {
-    //return ... ;
+    return db.data.blacklistUsers;
 }
+
+export { addBlacklist, getBlacklist, removeBlacklist };
